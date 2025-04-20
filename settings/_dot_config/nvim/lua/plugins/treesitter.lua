148fd8803eb7336f6e01667d48714e917652e5c9
Pulkit Sinha <itsapinhulk@outlook.com>
2025-04-20 15:45:12 -0400

    Add / remove supported languages

--- settings/_dot_config/nvim/lua/plugins/treesitter.lua
+++ settings/_dot_config/nvim/lua/plugins/treesitter.lua
@@ -7,9 +7,9 @@ return {
       configs.setup({
           ensure_installed = {
             "c", "cpp", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" ,
-            "markdown", "markdown_inline", "rust", "java", "go", "python", "javascript", "swift",
-            "typescript", "html", "css", "matlab", "bash", "cmake", "csv", "cuda", "sql", "tsx",
-            "yaml", "kotlin", "json", "latex",
+            "markdown", "markdown_inline", "rust", "java", "go", "python", "javascript", "tsx",
+            "typescript", "html", "css", "matlab", "bash", "cmake", "csv", "cuda", "sql",
+            "yaml", "kotlin", "json", "diff", "asm", "haskell", "ini", "toml", "wgsl", "glsl",
           },
           auto_install = false,
           sync_install = false,
