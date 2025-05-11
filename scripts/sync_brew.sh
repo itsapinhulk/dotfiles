3daa586932fab7e4b8cfa004ac085e1310c09bca
Pulkit Sinha <itsapinhulk@outlook.com>
2025-05-11 12:34:57 -0400

    Add script for homebrew and prefer it for completions

---
+++ scripts/sync_brew.sh
@@ -0,0 +1,19 @@
+brew update &&
+brew install \
+  bash \
+  bash-completion@2 \
+  fd \
+  git \
+  git-lfs \
+  jj \
+  miniconda \
+  neovim \
+  nvm \
+  ripgrep \
+  the_silver_searcher \
+  tmux \
+  tmuxp \
+  tpm \
+  &&
+brew upgrade &&
+brew autoremove
