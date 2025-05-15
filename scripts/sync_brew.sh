152b55bf3fd68c21560e4876d362fd39f251f474
Pulkit Sinha <itsapinhulk@outlook.com>
2025-05-14 21:50:45 -0400

    Add more packages and use quiet mode

--- scripts/sync_brew.sh
+++ scripts/sync_brew.sh
@@ -1,21 +1,28 @@
-brew update &&
-brew install \
+brew update --quiet &&
+brew install --quiet \
   bash \
   bash-completion@2 \
+  cmake \
+  curl \
   fd \
   fzf \
   git \
   git-lfs \
+  ipython \
   jj \
+  jupyterlab \
   lua \
   luarocks \
   neovim \
+  ninja  \
   nvm \
   ripgrep \
   the_silver_searcher \
   tmux \
   tmuxp \
   tpm \
+  wget \
+  zip \
   &&
 brew upgrade &&
 brew autoremove
